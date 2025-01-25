cda735f215289b0955f6f216227bd66a29c0cbe7
Suyash Kumar <suyashkumar2005@gmail.com>
2025-01-25 21:55:29 +0530

    Fixed file upload

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,14 +1,7 @@
 const multer = require('multer');
 
 // Set up storage for uploaded files
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
-  }
-});
+const storage = multer.memoryStorage();
 
 // Create the multer instance
 const upload = multer({ storage: storage });
